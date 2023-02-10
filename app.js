42a3cf8b8c22f89cda65596af4f2ffedc4f3cba5
selnoron <124687107+selnoron@users.noreply.github.com>
2023-02-10 20:49:03 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+
+// // 1
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// console.log(
+//     "Perimetr - " 
+//     + a * b 
+//     + "\nPloshad - " 
+//     + (a + b) * 2
+//     )
+
+// // 2
+// let a = parseInt(prompt())
+// console.log("Deametr - " + a * 2)
+
+// // 3
+// let konf = 15
+// let pech = 12
+// let yabl = 7
+// let k = parseInt(prompt("Konfet ?"))
+// let p = parseInt(prompt("Pechenya ?"))
+// let y = parseInt(prompt("Yablok ?"))
+// console.log(k * konf + p * pech + y * yabl)
+
+// // 4
+// let v1 = parseInt(prompt())
+// let v2 = parseInt(prompt())
+// let s = parseInt(prompt())
+// console.log((s / (v1 + v2)).toFixed(1) + " hour")
+
+// // 5
+// let metr = parseInt(prompt())
+// console.log((metr / 1000).toFixed() + " km")
+
+// // 6
+// let n = parseInt(prompt())
+// console.log((n % 12) + 1 + " - x")
+
+// // 7
+// let n = parseInt(prompt())
+// console.log("a - " + n % 10)
+// console.log("b - " + parseInt(n / 10) % 10)
+// console.log("c - " + (n % 10 + parseInt(n / 10) % 10 + parseInt(n / 100)))
+// console.log("d - " + n % 10 * parseInt(n / 10) % 10 * parseInt(n / 100))
+
+// // 8
+// let n = parseInt(prompt())
+// console.log(
+//     "hour - " + (n * 120) / 3600
+//     + "\nminuts - " + ((n * 120) / 3600) * 60
+// )
---
+++ app2.js
@@ -0,0 +1,90 @@
+// // 1
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// if (a / 0.0003048 > b) {
+//     console.log(b)
+// }
+// else if (a / 0.0003048 == b) {
+//     console.log(a + b)
+// }
+// else {
+//     console.log(a)
+// }
+
+// // 2
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// if (b >= a){
+//     console.log("Yes")
+// }
+// else{
+//     console.log("No")
+// }
+
+// // 3
+// let a = parseInt(prompt())
+// if (a % 2 == 0) {
+//     console.log("chet")
+// }
+// else {
+//     console.log("nechet")
+// }
+
+// if (a == 7) {
+//     console.log("sem")
+// }
+// else {
+//     console.log("nesem")
+// }
+
+// // 4
+// let a = parseInt(prompt("а: "))
+// let ch = parseInt(prompt("ch: "))
+// let gg = parseInt((((ch / 10) % 10) + ch % 10))
+// if (gg > 9 && gg < 100) {
+//     console.log("a) true")
+// }
+// else {
+//     console.log("a) false")
+// }
+// if (gg == a) {
+//     console.log("b) sum = a")
+// }
+// else if(gg > a){
+//     console.log("b) sum > a")
+// }
+// else {
+//     console.log("b) a > sum")
+// }
+
+// // 5
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// let x = parseInt(prompt())
+// let y = parseInt(prompt())
+// if (a - x >= 1 && b - y >= 1){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+// // 6
+// let a = parseInt(prompt("а: "))
+// let ch = parseInt(prompt("ch: "))
+// let h1 = parseInt((ch / 10) % 10)
+// let h2 = ch % 10
+// if (h1 == 3 || h2 == 3)
+// {
+//     console.log("3 in cheslo")
+// }
+// else{
+//     console.log("no 3 in cheslo")
+// }
+// if (h1 == a || h2 == a)
+// {
+//     console.log("A in cheslo")
+// }
+// else{
+//     console.log("no A in cheslo")
+// }
